07677f5eeb1e00a7be827f6d4a046aab8ab17cb5
Grant Glidewell <grantg@Grants-MacBook-Pro.local>
2017-08-01 21:23:55 -0700

    time updated

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 let countdown
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
-
 const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
@@ -38,14 +37,12 @@ function displayEndTime(timestamp) {
 }
 function startTimer() {
 	const seconds = parseInt(this.dataset.time);
-	console.log(seconds);
 	timer(seconds);
 }
 buttons.forEach(button => button.addEventListener('click', startTimer))
 document.customForm.addEventListener('submit', function(e) {
 	e.preventDefault();
 	const mins = this.minutes.value;
-	console.log(mins);
 	timer(mins *60);
 	this.reset();
 });
